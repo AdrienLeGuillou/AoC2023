e8d28e02a1830c5014373542d9a4600593f91cb7
Adrien Le Guillou <git@aleguillou.org>
2023-12-11 11:35:23 +0100

    add day 11

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,153 +1,55 @@
 fn main() {
-    // Part 1
     let input = std::fs::read_to_string("input.txt").unwrap();
     let map = Map::parse_input(&input);
+    let galaxies = map.find_galaxies();
 
-    let pos_start = map.get_start();
-    let pipe_cur = get_start_pipe(&map, pos_start);
-    // let pipe_cur = Pipe::from_u8(4);
-    let mut pos_cur = pipe_cur.go_through(pos_start);
-    let mut pipe_prev = pipe_cur;
-    let mut n_steps = 1;
-    let mut area = 0_f64;
+    let mut empty_cols: Vec<usize> = Vec::new();
+    let mut empty_rows: Vec<usize> = Vec::new();
 
-    loop {
-        let pos_prev = pos_cur;
-        let pipe_cur = Pipe::from_u8(map.get(pos_cur).unwrap());
-        let pipe_cur = pipe_cur.align(&pipe_prev);
-        pos_cur = pipe_cur.go_through(pos_cur);
+    let mut n = 0;
+    for c in map.cols() {
+        if ! c.contains(&Space::Galaxy) {
+            empty_cols.push(n);
+        }
+        n += 1;
+    }
 
-        // shoelace algorith to calculate the area of the polygon
-        area += 0.5
-              * (pos_prev.1 as f64 + pos_cur.1 as f64)
-              * (pos_prev.0 as f64 - pos_cur.0 as f64);
-        pipe_prev = pipe_cur;
+    let mut n = 0;
+    for r in map.rows() {
+        if ! r.contains(&Space::Galaxy) {
+            empty_rows.push(n);
+        }
+        n += 1;
+    }
 
-        n_steps += 1;
-
-        if pos_cur.0 == pos_start.0 && pos_cur.1 == pos_start.1 {
-            break
+    let mut answ = 0;
+    for i in 0..galaxies.len() {
+        for j in (i + 1)..galaxies.len() {
+            let dist = galaxies[i].dist_exp(&galaxies[j], &empty_cols, &empty_rows);
+            answ += dist;
         }
     }
-    println!("Part 1: {}", n_steps / 2);
+    println!("Part1: {}", answ);
 
-    // pick's theorem for area of polygon's with integer vertex
-    let answ = area.abs() - (n_steps as f64) / 2.0 + 1.0;
-    println!("Part 2: {}", answ);
-}
-
-fn parse_pipe(c: char) -> u8 {
-    match c {
-        '-' => 0,
-        '|' => 1,
-        'L' => 2,
-        '7' => 3,
-        'J' => 4,
-        'F' => 5,
-        'S' => 6,
-        _ => 9,
-    }
-}
-
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-enum Dir { N, S, E, W }
-
-impl Dir {
-    fn to_move(&self) -> (i32, i32) {
-        match self {
-            Self::N => (-1, 0),
-            Self::S => (1, 0),
-            Self::E => (0, 1),
-            Self::W => (0, -1),
+    let mut answ = 0;
+    for i in 0..galaxies.len() {
+        for j in (i + 1)..galaxies.len() {
+            let dist = galaxies[i].dist_exp_old(&galaxies[j], &empty_cols, &empty_rows);
+            answ += dist;
         }
     }
-
-    fn reverse(&self) -> Self {
-        match self {
-            Self::N => Self::S,
-            Self::S => Self::N,
-            Self::E => Self::W,
-            Self::W => Self::E,
-        }
-    }
-
-    fn apply_move(&self, pos: (usize, usize)) -> (usize, usize) {
-        let dir = self.to_move();
-        ((pos.0 as i32 + dir.0) as usize, (pos.1 as i32 + dir.1) as usize)
-    }
-}
-
-#[derive(Debug)]
-struct Pipe {
-    entry: Dir,
-    exit: Dir,
-}
-
-impl Pipe {
-    fn new(entry: Dir, exit: Dir) -> Self {
-        Self {entry, exit}
-    }
-
-    fn reverse(self) -> Self {
-        Self {
-            entry: self.exit,
-            exit: self.entry,
-        }
-    }
-
-    fn from_u8(u: u8) -> Self {
-        match u {
-            0 => Self::new(Dir::W, Dir::E),
-            1 => Self::new(Dir::N, Dir::S),
-            2 => Self::new(Dir::N, Dir::E),
-            3 => Self::new(Dir::W, Dir::S),
-            4 => Self::new(Dir::N, Dir::W),
-            _ => Self::new(Dir::E, Dir::S),
-        }
-
-    }
-
-    fn align(self, prev: &Self) -> Self {
-        if self.entry == prev.exit.reverse() {
-            self
-        } else {
-            self.reverse()
-        }
-    }
-
-    fn go_through(&self, pos: (usize, usize)) -> (usize, usize) {
-        self.exit.apply_move(pos)
-    }
-
-    // is tile X connected to pipe from dir
-    fn is_connected(&self, dir: Dir) -> bool {
-        self.entry == dir.reverse() || self.exit == dir.reverse()
-    }
-}
-
-fn get_start_pipe(map: &Map, pos: (usize, usize)) -> Pipe {
-    let drs = [Dir::N, Dir::S, Dir::E, Dir::W];
-    let mut io = Vec::new();
-    for dir in drs {
-        let t_pos = dir.apply_move(pos);
-        if let Some(v) = map.get(t_pos) {
-            if Pipe::from_u8(v).is_connected(dir) {
-                io.push(dir);
-            }
-        }
-    }
-    Pipe::new(io[0], io[1])
+    println!("Part2: {}", answ);
 }
 
 #[derive(Debug)]
 struct Map {
-    m: Vec<u8>,
+    m: Vec<Vec<Space>>,
     n_cols: usize,
     n_rows: usize,
 }
 
 impl Map {
-    fn new(m: Vec<u8>, n_rows: usize) -> Self {
+    fn new(m: Vec<Vec<Space>>, n_rows: usize) -> Self {
         let n_cols = m.len() / n_rows;
         Self {
             m,
@@ -156,29 +58,196 @@ impl Map {
         }
     }
 
-    fn get(&self, pos: (usize, usize)) -> Option<u8> {
-        if pos.0 >= self.n_rows || pos.1 >= self.n_cols {
-            None
+    fn get(&self, pos: Position) -> Result<Space, &'static str> {
+        if pos.row >= self.n_rows || pos.col >= self.n_cols {
+            Err("Subscript out of bounds")
         } else {
-            Some(self.m[self.n_cols * pos.0 + pos.1])
+            Ok(self.m[self.n_cols * pos.row][pos.col])
         }
     }
 
     fn parse_input(s: &str) -> Self {
-        let mut map = Vec::new();
-        let mut n_rows = 0;
+        let mut m: Vec<Vec<Space>> = Vec::new();
         for l in s.lines() {
-            n_rows += 1;
-            for c in l.chars() {
-                map.push(parse_pipe(c));
-            }
+            m.push(l.chars().map(Space::parse).collect());
         }
-        Self::new(map, n_rows)
+        let n_rows = m.len();
+        let n_cols = m[0].len();
+        Self {
+            m,
+            n_cols,
+            n_rows,
+        }
     }
 
-    fn get_start(&self) -> (usize, usize) {
-        let pos = self.m.iter().position(|&x| x == 6).unwrap();
-        (pos / self.n_cols, pos % self.n_cols)
+    fn iter<'a>(&'a self) -> IterMap<'a> {
+        IterMap{
+            inner: self,
+            pos: Position::new(0, 0),
+        }
+    }
+
+    fn cols<'b>(&'b self) -> IterMapCols<'b> {
+        IterMapCols {
+            inner: self,
+            col: 0,
+        }
+    }
+
+    fn rows<'b>(&'b self) -> IterMapRows<'b> {
+        IterMapRows {
+            inner: self,
+            row: 0,
+        }
+    }
+
+    fn find_galaxies(&self) -> Vec<Position> {
+        let mut out = Vec::new();
+        for row in 0..self.n_rows {
+            for col in 0..self.n_cols {
+                if self.m[row][col] == Space::Galaxy {
+                    out.push(Position::new(row, col))
+                }
+            }
+        }
+        out
     }
 }
 
+struct IterMap<'a> {
+    inner: &'a Map,
+    pos: Position,
+}
+
+impl<'a> Iterator for IterMap<'a> {
+    type Item = &'a Space;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.pos.col < self.inner.n_cols - 1 {
+            self.pos.col += 1;
+        } else {
+            if self.pos.row < self.inner.n_rows - 1 {
+                self.pos.row += 1;
+                self.pos.col = 0;
+            } else {
+                return None
+            }
+        }
+        Some(&self.inner.m[self.pos.row][self.pos.col])
+    }
+}
+
+struct IterMapCols<'a> {
+    inner: &'a Map,
+    col: usize,
+}
+
+impl<'b> Iterator for IterMapCols<'b> {
+    type Item = Vec<Space>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.col >= self.inner.n_cols - 1 {
+            return None
+        }
+
+        let mut out = Vec::new();
+        for i in 0..self.inner.n_rows {
+            out.push(self.inner.m[i][self.col])
+        }
+        self.col += 1;
+        Some(out)
+    }
+}
+
+struct IterMapRows<'b> {
+    inner: &'b Map,
+    row: usize,
+}
+
+impl<'b> Iterator for IterMapRows<'b> {
+    type Item = Vec<Space>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.row >= self.inner.n_rows - 1 {
+            return None
+        }
+        let out = self.inner.m[self.row].clone();
+        self.row += 1;
+        Some(out)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Space {
+    Galaxy,
+    Empty,
+}
+
+impl Space {
+    fn parse(c: char) -> Self {
+        match c {
+            '#' => Self::Galaxy,
+            _ => Self::Empty,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Position {
+    row: usize,
+    col: usize,
+}
+
+impl Position {
+    fn new(row: usize, col: usize) -> Self {
+        Self {row, col}
+    }
+
+    fn dist(&self, other: &Self) -> usize {
+        let vert = (self.row as i64 - other.row as i64).abs();
+        let hori = (self.col as i64 - other.col as i64).abs();
+        (vert + hori) as usize
+    }
+
+    fn dist_exp(&self, other: &Self,
+                e_cols: &Vec<usize>, e_rows: &Vec<usize>) -> usize {
+        let vert = (self.row as i64 - other.row as i64).abs();
+        let hori = (self.col as i64 - other.col as i64).abs();
+
+        let dup_r = count_between(e_rows, self.row, other.row);
+        let dup_c = count_between(e_cols, self.col, other.col);
+
+
+        (vert + hori) as usize + dup_r + dup_c
+    }
+
+    fn dist_exp_old(&self, other: &Self,
+                e_cols: &Vec<usize>, e_rows: &Vec<usize>) -> u64 {
+        let vert = (self.row as i64 - other.row as i64).abs() as u64;
+        let hori = (self.col as i64 - other.col as i64).abs() as u64;
+
+        let dup_r = count_between(e_rows, self.row, other.row) as u64;
+        let dup_c = count_between(e_cols, self.col, other.col) as u64;
+
+
+        (vert + hori) + (dup_r + dup_c) * 999_999
+    }
+}
+
+fn between(x: &usize, a: usize, b: usize) -> bool {
+    if a > b {
+        *x > b && *x < a
+    } else {
+        *x > a && *x < b
+    }
+}
+
+fn count_between(v: &Vec<usize>, a: usize, b: usize) -> usize {
+    let mut count = 0_usize;
+    for elt in v {
+        if between(elt, a, b) {
+            count += 1;
+        }
+    }
+    count
+}
